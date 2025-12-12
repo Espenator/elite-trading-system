20215cca662b5a48c498f877e1bbab1de4310bd2
oleh-savenko <oleh@embodier.ai>
2025-12-12 11:00:11 +0200

    chore: Remove Header and Layout components from the frontend layout directory to streamline the codebase.

--- frontend/src/components/layout/Layout.jsx
+++
@@ -1,9 +0,0 @@
-export default function Layout({ children }) {
-  return (
-    <div className="min-h-screen flex justify-center font-rubik">
-      <main className="flex-1 w-full max-w-[650px] bg-light py-2.5 px-5">
-        {children}
-      </main>
-    </div>
-  );
-}
